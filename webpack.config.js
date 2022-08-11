5a59c00c8f3db9e72f23a9a700dbc3bf7d120cd1
FacundoBettella <facundobettella@outlook.com>
2022-08-11 18:00:26 -0300

    Se agrega .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 const path = require("path");
 
 module.exports = {
-    /* Punto de entrada a la aplicacion */
-    entry: "./src/index.js",
+  /* Punto de entrada a la aplicacion */
+  entry: "./src/index.js",
 
-   /* Punto de salida a la aplicacion: A donde enviamos lo que prepara webpack */
-   output: {
+  /* Punto de salida a la aplicacion: A donde enviamos lo que prepara webpack */
+  output: {
     path: path.resolve(__dirname, "dist"), // Seleccionamos el path/folder donde guardamos nuestro poryecto.
-    filename: "main.js" // Colocamos nombre al resultante del js unificado. Tambien suele nombrarse: bundle.js
-   },
-   
-   /* Establecemos la extenciones que debe identificar webpack para leerlas correctamente */
-   resolve :{
-    extensions: ["js", "jsx", "ts", "tsx"]
-   }
-}
+    filename: "main.js", // Colocamos nombre al resultante del js unificado. Tambien suele nombrarse: bundle.js
+  },
+
+  /* Establecemos la extenciones que debe identificar webpack para leerlas correctamente */
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+
+  /* Añadimos configuración y reglas para trabajar con webpack. */
+  module: {
+    rules: [
+      {
+        // Lee los archivos con extension .js e ignora node_modules
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
