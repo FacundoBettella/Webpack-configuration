607a187085a016e6b9b8d6efdd3503d0ad8d08c0
FacundoBettella <facundobettella@outlook.com>
2022-08-13 15:03:12 -0300

    Optimizacion hashes y mimificacion con css-minimizer y terser-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   /* Punto de entrada a la aplicacion */
@@ -11,7 +12,10 @@ module.exports = {
   /* Punto de salida a la aplicacion: A donde enviamos lo que prepara webpack */
   output: {
     path: path.resolve(__dirname, "dist"), // Seleccionamos el path/folder donde guardamos nuestro poryecto.
-    filename: "main.js", // Colocamos nombre al resultante del js unificado. Tambien suele nombrarse: bundle.js
+
+    // Para identificar cada build de nuestro proyecto con un build cambiamos el filename de "main.js" a "[name].[contenthash].js"
+    filename: "[name].[contenthash].js", // Colocamos nombre final al resultante del js unificado. Tambien suele nombrarse: bundle.js
+    assetModuleFilename: "assets/images/[hash][ext][query]" //Esta instrucción hace que webpack le agregue un hash ( un hash es una serie de caracteres aleatorios) y su extencion por medio de esas variables en el string
   },
 
   /* Establecemos la extenciones que debe identificar webpack para leerlas correctamente */
@@ -33,35 +37,59 @@ module.exports = {
       {
         // Revisa las dos extensiones css y styl para aplicar los loaders css y stylus
         test: /\.css|.styl$/i,
-        use: [
-            MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
       },
       {
         // Loader interno a webpack, no tenemos que instalarlo. Mejora nuestros archivos png, los convierte a B64
         test: /\.png/,
-        type: "asset/resource"
-      }
+        type: "asset/resource",
+      },
+      {
+        // Loader para optimizar el formato woff de las fonts
+        test: /\.(woff|woff2)%/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000, // O un bolean. Habilita o deshabilita la transformación de archivos en base64.
+            mimetype: "aplication/font-woff",  // Especifica el tipo MIME con el que se alineará el archivo. 
+            // Los MIME Types (Multipurpose Internet Mail Extensions). Son la manera standard de mandar contenido a través de la red.
+            name: "[name].[contenthash].[ext]",
+            outputPath: './assets/fonts/', 
+            publicPath: './assets/fonts/',
+            esModule: false, // Avisar explicitamente si es un modulo.
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        inject: true,
-        // Inyecta el bundle al template HTML
-        template: "./public/index.html",
-        // Nombre final del archivo
-        filename: "./index.html"
+      inject: true,
+      // Inyecta el bundle al template HTML
+      template: "./public/index.html",
+      // Nombre final del archivo
+      filename: "./index.html",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      // Movemos el css a los assets, conservar su nombre pero que genere un content hash.
+      filename: "assets/[name].[contenthash].css"
+    }),
 
     new CopyWebpackPlugin({
-        patterns: [
-            {
-                // Copiamos la carpeta src/assets/images y la creamos en dist/ (Previamente a correr run dev, cambiamos la fuente de las imagenes en Template.js)
-                from: path.resolve(__dirname, "src", "assets/images"),
-                to: "assets/images"
-            }
-        ]
-    })
-  ]
+      patterns: [
+        {
+          // Copiamos la carpeta src/assets/images y la creamos en dist/ (Previamente a correr run dev, cambiamos la fuente de las imagenes en Template.js)
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(), // Plugin de optimizacion para css.
+      new TerserPlugin(), // Plugin de optimizacion para javascrip.
+    ]
+  }
 };
