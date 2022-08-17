7a6591dc507af9e7257b7fc58cc85d9312418290
FacundoBettella <facundobettella@outlook.com>
2022-08-16 22:32:39 -0300

    Se agrega scripts/create-env y se modifica el script build en package.json

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,6 +1,6 @@
 const fs = require("fs"); // Fs es un modulo de NODE llamado fileSystem y nos permiten enviar datos en nuestra computadora.
 
-fs.writeFileSync("./.env", `API =${process.env.API}\n `) 
+fs.writeFileSync('./.env', `API=${process.env.API}\n`) 
 /* 
     Creamos un archivo .env en la raiz del proyecto. Y creamos este recurso .env del lado del servidor. 
     
